4be47798a1e11c3539945de3630739fdc6b8dc7b
Annie <annie_baines@hotmail.co.uk>
2022-09-15 16:07:24 +0100

    add previous entries paragraph

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ lapIncrement()
 
 console.log(lapsCompleted) */
 
+let saveEl = document.getElementById("save-el")
+
 let countEl = document.getElementById("dog-count")
 console.log(countEl)
 
 let dogCount = 0
 
 function increment() {
-    dogCount = dogCount +1
+    dogCount += +1
     countEl.innerText = dogCount
     console.log(dogCount)
 }
 
 function save() {
     console.log(dogCount)
+
 }
+
